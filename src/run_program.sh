eae3da876c1335af024fe0586929b50361a95890
Leanne <leannestranks86@hotmail.com>
2020-10-02 13:21:55 +1000

    Changed the cd file info on sh file

--- src/run_program.sh
+++ src/run_program.sh
@@ -2,6 +2,7 @@
 echo "Welcome to the Quiz Bites Application"
 echo "The install will be finished shortly"
 git clone "https://github.com/Leanne2603/LeanneStranks_T1A3"
+cd LeanneStranks_T1A3/src
 echo "Please wait whilst the gems are installed"
 bundle install
 echo "The application will run shortly"
